89b9c5fe029e9ed4cd44d7fd8b0bcc09a10625da
soufyane khalfallah <khalfalasoufi@gmail.com>
2024-03-26 15:44:44 +0300

    updates

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,6 +8,22 @@ import ui_ux from "./UI_UX.png";
 import Video_Editing from "./Video_Editing.png";
 import Web_Dev from "./Web_Dev.png";
 import Wireframe from "./Wireframe.png";
+import Bootstrap from "./Bootstrap.png";
+import ChatGPT from "./ChatGPT.png";
+import Css from "./CSS3.png";
+import Figma from "./Figma.png";
+import Firebase from "./Firebase.png";
+import Git from "./Git.png";
+import GitHub from "./GitHub.png";
+import Html from "./Html.png";
+import Java from "./Java.png";
+import JavaScript from "./JavaScript.png";
+import Material_Ui from "./Material_Ui.png";
+import PP from "./PP.png";
+import Python from "./Python.png";
+import react from "./React.png";
+import Tailwind from "./Tailwind.png";
+import Vs from "./Vs.png";
 export {
   logo,
   menu,
@@ -19,4 +35,20 @@ export {
   Video_Editing,
   Web_Dev,
   Wireframe,
+  Bootstrap,
+  ChatGPT,
+  Css,
+  Figma,
+  Firebase,
+  Git,
+  GitHub,
+  Html,
+  Java,
+  JavaScript,
+  Material_Ui,
+  PP,
+  Python,
+  react,
+  Tailwind,
+  Vs,
 };
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,104 @@
 import React from "react";
-
+import {
+  PP,
+  Java,
+  JavaScript,
+  react,
+  Material_Ui,
+  Python,
+  Git,
+  GitHub,
+  Html,
+  Vs,
+  Tailwind,
+  Firebase,
+  Figma,
+  Css,
+  ChatGPT,
+  Bootstrap,
+} from "../assets";
 const About = () => {
+  const skills = [
+    {
+      img: PP,
+      alt: "PP",
+    },
+    {
+      img: Java,
+      alt: "java",
+    },
+    {
+      img: JavaScript,
+      alt: "javaScript",
+    },
+    {
+      img: react,
+      alt: "react",
+    },
+    {
+      img: Material_Ui,
+      alt: "Material_Ui",
+    },
+    {
+      img: Python,
+      alt: "Python",
+    },
+    {
+      img: Git,
+      alt: "Git",
+    },
+    {
+      img: GitHub,
+      alt: "GitHub",
+    },
+    {
+      img: Html,
+      alt: "Html",
+    },
+    {
+      img: Vs,
+      alt: "Vs",
+    },
+    {
+      img: Tailwind,
+      alt: "Tailwind",
+    },
+    {
+      img: Firebase,
+      alt: "Firebase",
+    },
+    {
+      img: Figma,
+      alt: "Figma",
+    },
+    {
+      img: Css,
+      alt: "CSS3",
+    },
+    {
+      img: ChatGPT,
+      alt: "ChatGPT",
+    },
+    {
+      img: Bootstrap,
+      alt: "Bootstrap",
+    },
+  ];
   return (
     <section id="About" className="  rounded-3xl p-3  scroll-smooth ">
-      <div className="container">
-        <div className="mt-10">
-          <h2 className=" text-orange-500 font-bold text-[28px] text-center">
-            About Me
+      <div className="container py-6 flex flex-col md:flex-row items-center justify-between">
+        <div className="about ">
+          <div className="flex items-center">
+            <div className=" h-1 w-6 bg-main_color " />
+            <div className=" py-2 px-3 border-solid text-[24px] font-semibold text-main_dark">
+              About Me
+            </div>
+          </div>
+          <h2 className=" text-[32px] md:text-[42px] max-w-[400px] font-bold">
+            Who is <span className=" text-main_color">Soufyane Khalfallah</span>{" "}
+            ?
           </h2>
-        </div>
-        <div>
-          <p className=" text-[18px] leading-7 text-center mt-6">
+          <p className=" text-[16px] w-[350px] leading-7 mt-5  text-main_dark">
             Hey there! I'm Soufyane, a multidisciplinary creative soul
             passionate about weaving digital experiences and crafting visual
             stories. At 21 years old, I've embarked on a journey as a Front-End
@@ -19,7 +107,28 @@ const About = () => {
             constantly pushing boundaries to create compelling and immersive
             experiences.
           </p>
+          <button className=" bg-main_color text-white font-bold py-3 px-8 rounded-full   my-7">
+            Download CV
+          </button>
         </div>
+        <div className="skills max-w-[400px] max-h-[350px] flex flex-wrap my-5">
+          {skills.map((skill) => (
+            <div
+              key={skill.alt}
+              className=" w-[70px] bg-[#ab9fdc] hover:scale-110 hover:shadow transition-all  flex justify-center items-center m-2  rounded-full h-[60px]"
+            >
+              <img src={skill.img} alt={skill.alt} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="container py-6 flex flex-col md:flex-row  justify-between">
+        <div className="education">
+          <h3 className=" text-secondary_color text-[24px] font-semibold">
+            Education
+          </h3>
+        </div>
+        <div className="Work"></div>
       </div>
     </section>
   );
